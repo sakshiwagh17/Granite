69ddba472ef4a995316680d7e3521ff786cdbeb9
sakshiwagh17 <sakshikwagh17@gmail.com>
2025-10-20 18:47:35 +0000

    Setup ESBuild and Vite

--- app/models/task.rb
+++ app/models/task.rb
@@ -1,5 +1,5 @@
 class Task <ApplicationRecord 
     MAX_TITLE_LENGTH = 125
     VALID_TITLE_REGEX=/\A.*[a-zA-Z0-9].*\z/i
-    validates:title,presence:true, length: {maximum:125}, format: {with:VALID_TITLE_REGEX}
+    validates:title,presence:true, length: {maximum: MAX_TITLE_LENGTH}, format: {with:VALID_TITLE_REGEX}
 end
