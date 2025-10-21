65e38a166bb17bba7d5acad959158d8a81c9cca9
sakshiwagh17 <sakshikwagh17@gmail.com>
2025-10-21 16:18:27 +0000

    Setup Tailwind CSS

---
+++ tailwind.config.js
@@ -0,0 +1,10 @@
+/** @type {import('tailwindcss').Config} */
+module.exports = {
+  important: true,
+  content: ["./app/javascript/**/*.{js,jsx}", "./app/views/**/*.html.erb"],
+  theme: {
+    extend: {},
+  },
+  plugins: [],
+}
+
